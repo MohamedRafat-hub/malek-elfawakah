cd3fd257f348442972038cece59a83e0dd36296d
mohamedrafat <mtag66870@gmail.com>
2026-03-18 02:38:18 +0200

    create bottomNavBarEntity

---
+++ lib/features/home/domain/entities/bottom_navbar_entity.dart
@@ -0,0 +1,10 @@
+class BottomNavBarEntity {
+  final String activeIcon;
+  final String inActiveIcon;
+  final String title;
+
+  BottomNavBarEntity(
+      {required this.activeIcon,
+        required this.inActiveIcon,
+        required this.title});
+}
